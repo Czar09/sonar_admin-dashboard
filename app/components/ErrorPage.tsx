8e3d47b591af3465cac9c69dbe3ef8928f958ba0
tanuj <[artravellers98@gmail.com]>
2023-07-07 16:26:52 +0530

    admin: pages added!

--- app/components/ErrorPage.tsx
+++ app/components/ErrorPage.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BiErrorCircle } from 'react-icons/bi'
 
 interface ErrorPageProps {
     message?: string;
@@ -6,9 +7,10 @@ interface ErrorPageProps {
 
 const ErrorPage: React.FC<ErrorPageProps> = ({message}) => {
   return (
-    <div className=''>
+    <div className='px-8 md:px-4'>
         <div className='flex items-center justify-center h-[100vh]'>
-            <div>
+            <div className='flex justify-center items-center gap-1'>
+                <i className='text-5xl md:text-lg text-slate-500'><BiErrorCircle /></i>
                 <small className='text-slate-500 text-md'>{message}</small>
             </div>
         </div>
