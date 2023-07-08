97d78ce30596f37d21db9593a8656eafc1e0da7b
tanuj <[artravellers98@gmail.com]>
2023-07-08 14:08:13 +0530

    disp added

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Sonar Admin Dashboard',
-  description: '',
+  description: 'this is a sonar dashboard'
 }
 
 export default function RootLayout({
