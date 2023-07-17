c323de8fb2d87d964db741ddce23d4ab59978165
unknown <karankartikey72@gmail.com>
2023-07-17 18:04:57 +0530

    added product components realtime

--- app/components/OrdersComp.tsx
+++ app/components/OrdersComp.tsx
@@ -253,28 +253,3 @@ export default OrdersComp
 
 
 
-
-export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
-    // Create authenticated Supabase Client
-    const supabase = createServerSupabaseClient(ctx);
-    // Check if we have a session
-    const {
-        data: { session }
-    } = await supabase.auth.getSession();
-
-    if (session)
-        return {
-            redirect: {
-                destination: '/',
-                permanent: false
-            }
-        };
-
-    return {
-        props: {
-            initialSession: session,
-        }
-    };
-}
-
-
