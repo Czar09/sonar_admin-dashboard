3beb4136379af9fe743810a3d03299ac133eef88
unknown <karankartikey72@gmail.com>
2023-08-09 10:01:46 +0530

    fix: typo in Signin

--- app/components/SignIn.tsx
+++ app/components/SignIn.tsx
@@ -1,8 +1,5 @@
 "use client";
 import React, { useState, useEffect } from 'react'
-
-+
-
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Link from 'next/link';
