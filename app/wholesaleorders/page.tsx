c0b44337a185b6323ecb7ce14fbe0c069eb00acf
unknown <karankartikey72@gmail.com>
2023-08-24 05:39:46 +0530

    fix: don't knwo what

--- app/wholesaleorders/page.tsx
+++ app/wholesaleorders/page.tsx
@@ -4,34 +4,34 @@ import Sidebar from '../components/Sidebar'
 import WholesaleOrdersComp from '../components/WholesaleOrdersComp'
 import { useUser } from '@/utils/useUser'
 import { useRouter } from 'next/navigation';
+import WholesaleOrdersSpecificComp from '../components/WholesaleOrdersSpecificComp';
 
 const Index = () => {
-    const {isLoading, userDetails}= useUser();
+    const { isLoading, userDetails } = useUser();
     const router = useRouter();
 
-    if(isLoading){
+    if (isLoading) {
         return <div>Loading...</div>
     }
-    if(!userDetails){
-        router.push('/login')
-    }
-    if(userDetails?.role !='superAdmin' || userDetails?.role !='admin'){
-        router.push('/wholesaleorders/your-orders')
-    }
-  return (
-    <>
-    <div className='flex flex-col md:flex-row w-full  h-[100vh]'>
-        <div className=' lg:w-[20%]  '> {/*#131313*/}
-            <div className='md:h-[100vh] px-7 bg-gray-100 lg:fixed'>
-               <Sidebar />
+
+    return (
+        <>
+            <div className='flex flex-col md:flex-row w-full  h-[100vh]'>
+                <div className=' lg:w-[20%]  '> {/*#131313*/}
+                    <div className='md:h-[100vh] px-7 bg-gray-100 lg:fixed'>
+                        <Sidebar />
+                    </div>
+                </div>
+                {userDetails?.role == 'superAdmin' || userDetails?.role == 'admin' ?
+                    <div className='w-full '>
+                        <WholesaleOrdersComp />
+                    </div> :
+                    <div className='w-full '>
+                        <WholesaleOrdersSpecificComp />
+                    </div>}
             </div>
-        </div>
-        <div className='w-full '>
-            <WholesaleOrdersComp />
-        </div>
-    </div>
-</>
-)
+        </>
+    )
 }
 
 export default Index
